abfc354569768d2dd3c2dbf95efc23b8b0606d1f
Dhanesh C N E <93080135+dhanesh15@users.noreply.github.com>
2024-06-16 22:46:02 +0530

    Create Dynamic_Array.sv

---
+++ Dynamic_Array.sv
@@ -0,0 +1,25 @@
+module tb():
+
+int arr[];
+
+initial begin
+arr = new[7];
+
+for (int i = 0; i < 7; i++) begin
+arr[i] = (i+1)*7;
+end
+  
+$disp("Initial Array : %0p", arr);
+
+#20;
+
+arr = new[20](arr);
+
+for (int j = 7; j < 20; j++) begin
+arr[j] = (j - 6) * 5;
+end
+
+$disp("Result Array : %0p", arr);
+
+end
+endmodule
